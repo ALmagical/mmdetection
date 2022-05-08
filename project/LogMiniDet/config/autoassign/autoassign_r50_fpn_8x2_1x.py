1f62acdba74a178ab686a0ac38f511bfa0e9b635
ALmagical <1105420922@qq.com>
2022-05-08 20:37:42 +0800

    config mosic aug
    add mosic and mixup aug to autossign_r50

--- project/LogMiniDet/config/autoassign/autoassign_r50_fpn_8x2_1x.py
+++ project/LogMiniDet/config/autoassign/autoassign_r50_fpn_8x2_1x.py
@@ -1,9 +1,12 @@
 # We follow the original implementation which
 # adopts the Caffe pre-trained backbone.
+#_base_ = './autoassign_r50_fpn_8x2_1x_coco.py'
 _base_ = [
-    '../_base_/datasets/logminidet.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
+dataset_type = 'LogMiniDet'
+data_root = '/mnt/d/Github/mmdetection/project/LogMiniDet/data/0428/'
+img_scale = (1024, 1024)
 model = dict(
     type='AutoAssign',
     backbone=dict(
@@ -24,7 +27,8 @@ model = dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
         out_channels=256,
-        start_level=0,
+        norm_cfg=dict(type='GN', num_groups=32, requires_grad=True),
+        start_level=1,
         add_extra_convs=True,
         num_outs=5,
         relu_before_extra_convs=True,
@@ -35,6 +39,7 @@ model = dict(
         in_channels=256,
         stacked_convs=4,
         feat_channels=256,
+        dcn_on_last_conv=True,
         strides=[8, 16, 32, 64, 128],
         loss_bbox=dict(type='GIoULoss', loss_weight=5.0)),
     train_cfg=None,
@@ -46,19 +51,35 @@ model = dict(
         max_per_img=100))
 img_norm_cfg = dict(
     mean=[123.68, 116.779, 103.939], std=[58.393, 57.12, 57.375], to_rgb=True)
-train_pipeline = [
+load_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='RandomCrop', crop_size=(700, 700), crop_type='absolute'),
+    dict(type='LoadAnnotations', with_bbox=True)
+]
+train_pipeline = [
+    dict(type='Mosaic', img_scale=img_scale, pad_val=114.0),
     dict(
-        type='Resize',
-        img_scale=(700, 700),
-        ratio_range=(0.3, 1.5),
-        keep_ratio=True),
+        type='RandomAffine',
+        scaling_ratio_range=(0.1, 2),
+        border=(-img_scale[0] // 2, -img_scale[1] // 2)),
+    dict(
+        type='MixUp',
+        img_scale=img_scale,
+        ratio_range=(0.7, 1.8),
+        pad_val=114.0),
+    dict(type='YOLOXHSVRandomAug'),
     dict(type='Rotate', level=1, max_rotate_angle=30, prob=0.5),
     dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
+    # According to the official implementation, multi-scale
+    # training is not considered here but in the
+    # 'mmdet/models/detectors/yolox.py'.
+    dict(type='Resize', img_scale=img_scale, keep_ratio=True),
+    dict(
+        type='Pad',
+        pad_to_square=True,
+        # If the image is three-channel, the pad value needs
+        # to be set separately for each channel.
+        pad_val=dict(img=(114.0, 114.0, 114.0))),
+    dict(type='FilterAnnotations', min_gt_bbox_wh=(1, 1), keep_empty=False),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
@@ -77,12 +98,30 @@ test_pipeline = [
             dict(type='Collect', keys=['img'])
         ])
 ]
+train_dataset = dict(
+    type='MultiImageMixDataset',
+    dataset=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/train.json',
+        img_prefix=data_root + 'train/',
+        pipeline=load_pipeline,
+        filter_empty_gt=False),
+    pipeline=train_pipeline)
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=1,
-    train=dict(pipeline=train_pipeline),
-    val=dict(pipeline=test_pipeline),
-    test=dict(pipeline=test_pipeline))
+    train=train_dataset,
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/val.json',
+        img_prefix=data_root + 'val/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/val.json',
+        img_prefix=data_root + 'val/',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox')
 # optimizer
 optimizer = dict(lr=0.0025, paramwise_cfg=dict(norm_decay_mult=0.))
 # learning policy
@@ -91,9 +130,9 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=1000,
     warmup_ratio=1.0 / 1000,
-    step=[8, 11])
-total_epochs = 12
+    step=[16, 22])
+total_epochs = 24
 runner = dict(type='EpochBasedRunner', max_epochs=total_epochs)
 
-load_from = '/mnt/d/Github/mmdetection/project/LogDetMini/workdir/autoassign/baseline_2x/latest.pth'
-work_dir = '/root/autodl-tmp/code/mmdetection/project/LogMiniDet/work_dir/x101/pafpn_dcn_astt_mstrain_fulltrain'
+load_from = '/mnt/d/Github/mmdetection/project/LogMiniDet/workdir/autoassign/baseline_2x/epoch_24.pth'
+work_dir = '/mnt/d/Github/mmdetection/project/LogMiniDet/work_dir/autoassign/r50/nasfpn_dcn_mstrain'
--- project/LogMiniDet/config/autoassign/autoassign_s50_fpn_8x2_3x copy.py
+++ project/LogMiniDet/config/autoassign/autoassign_s50_fpn_8x2_3x copy.py
@@ -7,23 +7,26 @@ _base_ = [
 model = dict(
     type='AutoAssign',
     backbone=dict(
-        type='ResNet',
+        type='ResNeSt',
+        stem_channels=128,
         depth=50,
+        radix=2,
+        reduction_factor=4,
+        avg_down_stride=True,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
-        norm_cfg=dict(type='GN', num_groups=32, requires_grad=True),
-        norm_eval=True,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=False,
         style='pytorch',
-        init_cfg=dict(
-            type='Pretrained',
-            checkpoint='open-mmlab://detectron/resnet50_gn'),
+        init_cfg=dict(type='Pretrained',
+                      checkpoint='open-mmlab://resnest50'),
         dcn=dict(type='DCNv2', deform_groups=1, fallback_on_stride=False),
         stage_with_dcn=(False, True, True, True)),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
-        out_channels=256,
+        out_channels=160,
         start_level=0,
         add_extra_convs=True,
         num_outs=5,
@@ -32,10 +35,10 @@ model = dict(
     bbox_head=dict(
         type='AutoAssignHead',
         num_classes=50,
-        in_channels=256,
+        in_channels=160,
         stacked_convs=4,
-        feat_channels=256,
-        strides=[8, 16, 32, 64, 128],
+        feat_channels=160,
+        strides=[4, 8, 16, 32, 64],
         loss_bbox=dict(type='GIoULoss', loss_weight=5.0)),
     train_cfg=None,
     test_cfg=dict(
@@ -78,22 +81,22 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
+    samples_per_gpu=1,
     workers_per_gpu=1,
     train=dict(pipeline=train_pipeline),
     val=dict(pipeline=test_pipeline),
     test=dict(pipeline=test_pipeline))
 # optimizer
-optimizer = dict(lr=0.0025, paramwise_cfg=dict(norm_decay_mult=0.))
+optimizer = dict(lr=0.00125, paramwise_cfg=dict(norm_decay_mult=0.))
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=1000,
     warmup_ratio=1.0 / 1000,
-    step=[8, 11])
-total_epochs = 12
+    step=[24, 33])
+total_epochs = 36
 runner = dict(type='EpochBasedRunner', max_epochs=total_epochs)
 
-load_from = '/mnt/d/Github/mmdetection/project/LogDetMini/workdir/autoassign/baseline_2x/latest.pth'
-work_dir = '/root/autodl-tmp/code/mmdetection/project/LogMiniDet/work_dir/x101/pafpn_dcn_astt_mstrain_fulltrain'
+#load_from = '/mnt/d/Github/mmdetection/project/LogDetMini/workdir/autoassign/baseline_2x/latest.pth'
+work_dir = '/mnt/d/Github/mmdetection/project/LogMiniDet/work_dir/autoassign/s50/fpn_dcn_mstrain'
